e6a0916eab6aadca9db87ce024ffc0c10ab3ffdd
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-11-26 11:58:57 -0500

    Add seed

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,4 @@
 class Ticket < ApplicationRecord
-  belongs_to :event
   belongs_to :status
   has_many :ticket_logs, dependent: :destroy
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,22 @@ end
 
 # Fetch all statuses
 statuses = Status.all
+existing_serials = Set.new(Ticket.pluck(:serial_ticket)) # Cargar seriales únicos desde la base de datos
 
 # Generate 1000 tickets and ticket logs
 1000.times do
+  begin
+    serial_ticket = SecureRandom.hex(8)
+    puts(serial_ticket)
+  end while existing_serials.include?(serial_ticket) # Generar hasta que sea único
+
+  existing_serials.add(serial_ticket)
+
   ticket = Ticket.create!(
-    event_id: Faker::Number.between(from: 1, to: 1000), # Event IDs from external API
-    expire_date: Faker::Date.forward(days: 365),       # Random future date
-    status_id: statuses.sample.id,                    # Random status
-    serial_ticket: SecureRandom.hex(8),               # Unique serial
+    event_id: Faker::Number.between(from: 1, to: 20), # Fixed range of event IDs
+    expire_date: Faker::Date.forward(days: 365),      # Random future date
+    status_id: statuses.sample.id,                   # Random status
+    serial_ticket: serial_ticket,                    # Unique serial
     created_at: Time.now,
     updated_at: Time.now
   )
