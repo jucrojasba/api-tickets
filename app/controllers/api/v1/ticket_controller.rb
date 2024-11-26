6cf068d3af3b6f69657a41095c95516a97d22dc3
Daws <castrogiraldosantiago@gmail.com>
2024-11-26 08:42:51 -0500

    Fix: addition of all the extra fields that may be returned

--- app/controllers/api/v1/ticket_controller.rb
+++ app/controllers/api/v1/ticket_controller.rb
@@ -1,16 +1,17 @@
 class Api::V1::TicketController < ApplicationController
   def summary
     event_id = params[:event_id]# params
-
     # searching
     tickets = Ticket.where(event_id: event_id)
 
     # json REsponse
-    render json: {# TODOAdd ticket required calcs
-      event_id: event_id,
-      total_tickets: tickets.count,
-      sold_tickets: tickets.where(status: "sold").count,
-      available_tickets: tickets.where(status: "available").count
+    render json: {# Add ticket required calcs
+    event_id: event_id,
+    available_tickets: tickets.where(status: "available").count,
+    reserved_tickets: tickets.where(status: "reserved").count,
+    sold_tickets: tickets.where(status: "sold").count,
+    canceled_tickets: tickets.where(status: "canceled").count,
+    total_tickets: tickets.count
     }, status: :ok
   rescue ActiveRecord::RecordNotFound
     render json: { error: "Event not found" }, status: :not_found
