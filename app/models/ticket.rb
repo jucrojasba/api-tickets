df6e348ef113ab525cee555e813543cef699ab02
Daws <castrogiraldosantiago@gmail.com>
2024-11-26 08:45:05 -0500

    fix: model param requirement

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,7 +2,7 @@ class Ticket < ApplicationRecord
   belongs_to :status
   has_many :ticket_logs
 
-  validates :event_id, presence: true
+  validates :event_id, presence: true, numericality: { greater_than: 0 }, allow_nil: false
   validates :expire_date, presence: true
   # TODO(optional) add scpoe for search of the controller
 end
