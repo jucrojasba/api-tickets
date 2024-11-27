890c3a775597a98bc2c2d702f21c8ffc19adddec
HernanDavidA <alvaremoreno111@gmail.com>
2024-11-27 14:17:21 -0500

    feature/create-tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,8 +1,9 @@
 class TicketsController < ApplicationController
   # GET /tickets/:ticket_id/logs
+
+  skip_before_action :verify_authenticity_token, only: [ :create ]
   def logs
     @ticket = Ticket.find_by(id: params[:id])
-
     if @ticket
       logs = @ticket.ticket_logs.select(:id, :status_id, :created_at, :updated_at)
       render json: {
@@ -19,7 +20,6 @@ class TicketsController < ApplicationController
       render json: { error: "Ticket not found" }, status: :not_found
     end
   end
-
   # PATCH/PUT /tickets/:ticket_id/update_status
   def update_status
     ticket = Ticket.find_by(id: params[:ticket_id])
@@ -69,11 +69,40 @@ class TicketsController < ApplicationController
     render json: { error: "Event not found" }, status: :not_found
   end
 
-  private
-  def set_ticket
-    @ticket = Ticket.find_by(id: params[:id])
+  def create
+    event_id = params[:event_id]
+    @ticket_data = Ticket.get_data(event_id)
+
+    if @ticket_data.nil?
+      render json: { error: "Event not found" }, status: :not_found
+      return
+    end
+
+    ticket_quantity = @ticket_data["data"]["ticket_quantity"].to_i
+    event_capacity = @ticket_data["capacity"].to_i
+
+    if ticket_quantity > event_capacity
+      render json: { error: "Capacity exceeded" }, status: :unprocessable_entity
+      return
+    end
+
+    ticket_quantity.times do
+      ticket = Ticket.new(event_id: event_id)
+
+      if ticket.save
+        # Optionally, you can log each created ticket or any other logic you want
+      else
+        render json: { errors: ticket.errors.full_messages }, status: :unprocessable_entity
+        return
+      end
+    end
+
+    render json: { message: "#{ticket_quantity} tickets created successfully", event_id: event_id }, status: :created
   end
 
+
+
+  private
   def can_update_status?(ticket, new_status)
     return false if ticket.status == new_status
 
--- db/migrate/20241126154447_drop_events.rb
+++ db/migrate/20241126154447_drop_events.rb
@@ -1,5 +1,5 @@
 class DropEvents < ActiveRecord::Migration[7.2]
   def change
-    drop_table :events
+    drop_table :events if table_exists?(:events)
   end
 end
