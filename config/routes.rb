ab774a683d609047e6213255e219620207956198
Daws <castrogiraldosantiago@gmail.com>
2024-11-27 20:03:17 -0500

    Feat: adding route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # optional rout for the moment attached to changes
 
   get "events/:event_id/tickets/summary", to: "tickets#summary"
+  get "events/:event_id/tickets/:quantity", to: "tickets#reserve_tickets"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
