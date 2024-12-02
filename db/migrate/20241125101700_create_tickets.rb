624f64b90c11db5b0174b727f70a2a4b2e9fd100
Daws <castrogiraldosantiago@gmail.com>
2024-12-02 07:11:28 -0500

    fix: adaptation to db

--- db/migrate/20241125101700_create_tickets.rb
+++ db/migrate/20241125101700_create_tickets.rb
@@ -2,7 +2,7 @@ class CreateTickets < ActiveRecord::Migration[7.0]
   def change
     create_table :tickets do |t|
       t.integer :event_id, null: false
-      t.date :expire_date, null: false, default: -> { "DATE('now', '+30 days')" }
+      t.date :expire_date, null: false, default: -> { "CURRENT_DATE + interval '30 days'" }
       t.references :status, null: false, foreign_key: true
       t.integer :serial_ticket, null: true # deprecated
 
